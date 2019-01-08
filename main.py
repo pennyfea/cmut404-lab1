3c8d8988d2f8d85463f3cca16310848df825eb14
pennyfea <pennyfea@ualberta.ca>
2019-01-08 11:20:03 -0800

    Lab 1

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 import requests
 print(requests.__version__)
 test = requests.get("http://www.google.com/")
-print(test)
+raw = requests.get(
+    "https://raw.githubusercontent.com/pennyfea/cmut404-lab1/master/main.py")
+# print(dir(test))
+print(raw.text)
